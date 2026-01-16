fdda6198c7894ec6bfcc6f2958493fbf2af81088
Meski Abdelilah <meskiabdelilah10@gmail.com>
2026-01-16 23:16:00 +0100

    gitignore

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once './config/config.php';
+use App\core\Router;
+use App\controllers\front\UserController;
+use App\core\Model;
+
+// Create router instance
+$router = Router::getRouter();
+// Register the routes
+$router->get('/user/{user}/product/{product}',[UserController::class,'Show']);
+
+// Dispatch the router
+$router->dispatch();
+$userModel = new App\models\User();
+
+$user = $userModel->all();
+//   
