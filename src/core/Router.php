58a1346c1c931f93d90676f9cfeff4d56e7e9744
Meski Abdelilah <meskiabdelilah10@gmail.com>
2026-01-16 16:27:43 +0100

    feat(core): add router.php

---
+++ src/core/Router.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\core;
+
+class Router
+{
+    // array for storage the url
+    private static array $routes = [];
+    // This is a core part of the Singleton pattern to ensure only one
+    private static  $router;
+
+    private function __construct() {}
+
+    // for make sure there is a connection.
+    public static function getRouter(): router
+    {
+        if (!isset(self::$router)) {
+            self::$router = new self();
+        }
+        return self::$router;
+    }
+
+    private function register(string $route, string $method, array|callable $action)
+    {
+        $route = trim($route, '/');
+
+        // Assign action to the passed route
+        self::$routes[$method][$route] = $action;
+    }
+
+    public function get(string $route, array|callable $action)
+    {
+        $this->register($route, 'GET', $action);
+    }
+
+    public function post(string $route, array|callable $action)
+    {
+        $this->register($route, 'POST', $action);
+    }
+
+    public function put(string $route, array|callable $action)
+    {
+        $this->register($route, 'PUT', $action);
+    }
+
+    public function delete(string $route, array|callable $action)
+    {
+        $this->register($route, 'DELETE', $action);
+    }
+
+    // Resolve the current request to the corresponding route action.
+
+    public function dispatch()
+    {
+        $requestRoute = trim(
+            parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH),
+            '/'
+        );
+        $routes = self::$routes[$_SERVER['REQUEST_METHOD']];
+
+        foreach ($routes as $route => $action) {
+            $routeRegex = preg_replace_callback('/{\w+(:([^}]+))?}/', function ($matches) {
+                return isset($matches[2]) ? '(' . $matches[2] . ')' : '([a-zA-Z0-9_-]+)';
+            }, $route);
+
+            // Add the start and end delimiters.
+            $routeRegex = '@^' . $routeRegex . '$@';
+
+            // Check if the requested route matches the current route pattern.
+            if (preg_match($routeRegex, $requestRoute, $matches)) {
+                array_shift($matches);
+
+                // Get all user requested path params values after removing the first matches.  
+                $routeParamsValues = $matches;
+
+                // Find all route params names from route and save in $routeParamsNames
+                $routeParamsNames = [];
+                if (preg_match_all('/{(\w+)(:[^}]+)?}/', $route, $matches)) {
+                    $routeParamsNames = $matches[1];
+                }
+
+                // Combine between route parameter names and user provided parameter values.
+                $routeParams = array_combine($routeParamsNames, $routeParamsValues);
+
+                return $this->resolveAction($action, $routeParams);
+            }
+        }
+        return $this->abort('404 Page not found');
+    }
+
+    private function resolveAction($action, $routeParams)
+    {
+        if (is_callable($action)) {
+            return call_user_func_array($action, $routeParams);
+        } else if (is_array($action)) {
+            return call_user_func_array([new $action[0], $action[1]], $routeParams);
+        }
+    }
+    private function abort(string $message, int $code = 404)
+    {
+
+        http_response_code($code);
+        echo $message;
+        exit();
+    }
+}
