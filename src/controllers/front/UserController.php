6bbd67b978d32ce8d383c1a5d3d88ca49748b4fc
Meski Abdelilah <meskiabdelilah10@gmail.com>
2026-01-16 17:12:29 +0100

    feat(controllers): add UserConstroller.php

---
+++ src/controllers/front/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\controllers\front;
+
+use App\core\Controller;
+
+class UserController extends Controller
+{
+    public function Show($user, $product)
+    {
+        $this->render('test', [
+            'user' => $user,
+            'product' => $product
+        ]);
+    }
+}
