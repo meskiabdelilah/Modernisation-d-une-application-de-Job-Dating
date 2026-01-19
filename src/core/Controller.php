95725297a2736674dfba3ed27a5994426d4f3ea2
Meski Abdelilah <meskiabdelilah10@gmail.com>
2026-01-19 16:01:41 +0100

    feat(core/controller): Security improvement

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -1,20 +1,67 @@
 <?php
 
 namespace App\core;
-
-use App\core\Router;
-
+ 
 abstract class Controller
 {
+
+    protected $view;
+    protected $security;
+    protected $session;
+    protected $validator;
+
+    public function __construct()
+    {
+        $this->view = new View();
+        $this->security = new Security();
+        $this->session = Session::getInstance();
+        $this->validator = new Validator();
+    }
+
     protected function render($view, $data = [])
     {
-        extract($data);
-        $path =__DIR__ . '/../views/' . $view . '.php';
+        foreach ($data as $key => $value) {
+            $$key = $value;
+        }
+        $view = str_replace(['../', '..\\', '//', '\\\\'], '', $view);
+        $view = trim($view, '\//');
 
-        if (file_exists($path)) {
+        $viewsDir = realpath(__DIR__ . '/../views/');
+        $path =  $viewsDir . DIRECTORY_SEPARATOR . $view . '.php';
+
+        $realPath = realpath($path);
+        if ($realPath && strpos($realPath, $viewsDir) === 0 && file_exists($path)) {
             require_once $path;
-        }else{
-            die('View ' . $view . 'not found' . $path);
+        } else {
+            throw new \Exception('View ' . $view . ' not found');
+        }
+    }
+
+    protected function redirect(string $url, int $statusCode = 302)
+    {
+        http_response_code($statusCode);
+        header("Location: $url");
+        exit();
+    }
+
+    protected function twigView($view, $data = [])
+    {
+        View::twigView($view, $data);
+    }
+
+    protected function json(array $data, int $statusCode = 200)
+    {
+        http_response_code($statusCode);
+        header('Content-Type: application/json');
+        echo json_encode($data);
+        exit();
+    }
+
+    protected function verifyCsrf()
+    {
+        if (!$this->security->verifyCsrfToken($_POST['_token'] ?? '')) {
+            $this->json(['error' => 'Invalid CSRF token'], 403);
+            exit();
         }
     }
 }
